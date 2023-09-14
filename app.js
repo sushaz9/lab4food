5d4c07d3b582fca02f97277db8407585872ac8b4
sushaz9 <susannahelisabeth1@gmail.com>
2023-09-14 15:05:26 +0100

    editjs

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 console.log("Hello!!");
 
-let username = prompt("What is your name?");
-console.log("Hello" + username);
-
-//concatenation
-//console.log("Hello" + username);
-
-let string = prompt("Is Haggis from Scotland?");
-console.log("response");
-
-
-let score = prompt("How much do you like Haggis out of 10 (0-10)");
-
-let message;
-if (score < 5) {
-    message = "Ouch";
-} else if (score > 5) {
-    message = "Mmm!";} 
- else {
-message = "WRONG!";
+function greet() {
+    let username = prompt("What is your name?");
+    
+    document.write(username);
 }
 
-message = "Hello" + username + "." + message;
-document.write(message);
+function questionOne() {
+let message = prompt("Haggis is from Scotland (Yes or No)");
+if (message === "yes") {
+
+} else if (message === "no") {
+    
+} else {
+    (message = "Try again");
+}
+console.log("yes")
+}
+
+function questionTwo() {
+let answer = prompt("How much do you like Haggis out of 10 (0-10)");
+} if (answer < 5) {
+    answer = "Ouch";
+} else if (answer > 5) {
+    answer = "Mmm!";} 
+ else {
+answer = "WRONG!";
+}
